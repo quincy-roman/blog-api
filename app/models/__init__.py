8031cddb211d432f66b41ffe9a8ab368a83ea85a
Quincy Roman <qjroman@gmail.com>
2022-02-21 01:26:47 -0500

    Setup full application with beanie, removed pyObjectId and am using MongoDB's ObjectId

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,5 +1,4 @@
 from .blog import Blog, Comment, UpdateBlog
-from .pyObjectId import PyObjectId
 from .token import (ACCESS_TOKEN_EXPIRE_MINUTES, ALGORITHM, SECRET_KEY, Token,
                     TokenData)
 from .users import User, UserInDB
--- app/models/blog.py
+++ app/models/blog.py
@@ -6,16 +6,45 @@ from pydantic import BaseModel, Field
 
 
 class Comment(BaseModel):
-    author: str = Field(...)
+    author: Optional[str] = None
     body: str = Field(...)
 
 
-class Blog(Document):
+class UpdateBlog(BaseModel):
     author: Optional[str]
-    title: str = Field(...)
-    body: str = Field(...)
-    published: bool = Field(...)
-    comments: Optional[List[Comment]] = []
+    title: Optional[str]
+    body: Optional[str]
+    published: Optional[bool]
+    comments: Optional[List[Comment]]
+
+    class Config:
+        arbitrary_types_allowed = True
+        json_encoders = {ObjectId: str}
+        schema_extra = {
+            "example": {
+                "title": "Blog Title",
+                "body": "Content of the blog post",
+                "published": True,
+                "comments": [
+                    {
+                        "author": "Other Username",
+                        "body": "This is the comment body."
+                    },
+                    {
+                        "author": "anothaUser",
+                        "body": "This is another comment body."
+                    }
+                ]
+            }
+        }
+
+
+class Blog(Document, UpdateBlog):
+
+    @classmethod
+    async def by_id(cls, id: str) -> "Blog":
+        """Query by _id"""
+        return await cls.find_one(Blog.id == id)
 
     class Collection:
         name = 'blogs'
@@ -37,38 +66,6 @@ class Blog(Document):
                     },
                     {
                         "author": "another.Username",
-                        "body": "This is another comment body."
-                    }
-                ]
-            }
-        }
-
-
-class UpdateBlog(Document):
-    author: Optional[str]
-    title: Optional[str]
-    body: Optional[str]
-    published: Optional[bool]
-    comments: Optional[List[Comment]]
-
-    class Collection:
-        name = 'blogs'
-
-    class Config:
-        arbitrary_types_allowed = True
-        json_encoders = {ObjectId: str}
-        schema_extra = {
-            "example": {
-                "title": "Blog Title",
-                "body": "Content of the blog post",
-                "published": True,
-                "comments": [
-                    {
-                        "author": "Other Username",
-                        "body": "This is the comment body."
-                    },
-                    {
-                        "author": "anothaUser",
                         "body": "This is another comment body."
                     }
                 ]
